1e891c52335d970f06ed7eb01fbca31d64897f65
Jacques-Selaire31 <serra.victor@yahoo.fr>
2025-09-23 16:58:32 +0200

    Deuxième journée

--- src/app/exos/event-biding/event-biding.ts
+++ src/app/exos/event-biding/event-biding.ts
@@ -1,8 +1,12 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import {FormsModule} from '@angular/forms'
+
+// -------VERSION AVEC NG MODEL-----------//
 
 @Component({
   selector: 'app-event-biding',
-  imports: [],
+  imports: [FormsModule, NgIf],
   templateUrl: './event-biding.html',
   styleUrl: './event-biding.css'
 })
@@ -13,10 +17,31 @@ export class EventBiding {
   nameFriend = "";
   hasFriend: boolean = false;
   isDisabled: boolean = false;
-  updatedField(event: KeyboardEvent): void {
-    this.nameFriend += event.key;
-  }
+
   addFriend(event: PointerEvent): void {
     this.hasFriend = true;
   }
 }
+
+
+// -------VERSION SANS NG MODEL-----------//
+// @Component({
+//   selector: 'app-event-biding',
+//   imports: [],
+//   templateUrl: './event-biding.html',
+//   styleUrl: './event-biding.css'
+// })
+// export class EventBiding {
+//   constructor() {
+//     setTimeout(()=>{this.isDisabled = !this.isDisabled}, 5000);
+//   }
+//   nameFriend = "";
+//   hasFriend: boolean = false;
+//   isDisabled: boolean = false;
+//   updatedField(event: KeyboardEvent): void {
+//     this.nameFriend += event.key;
+//   }
+//   addFriend(event: PointerEvent): void {
+//     this.hasFriend = true;
+//   }
+// }
--- src/app/exos/property-biding/property-biding-exo.ts
+++ src/app/exos/property-biding/property-biding-exo.ts
@@ -1,16 +1,90 @@
+import { NgClass, NgFor, NgStyle } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+interface Friend {
+  img: string;
+  name: string;
+  statut: boolean;
+  age: number;
+  bio: string;
+  reputation: string;
+}
 
 @Component({
   selector: 'app-property-biding-exo',
-  imports: [],
+  imports: [FormsModule, NgClass, NgStyle, NgFor],
   templateUrl: './property-biding-exo.html',
   styleUrl: './property-biding-exo.css'
 })
 export class PropertyBidingExo {
-  img:string="https://picsum.photos/200/300"
-  name:string = "John Doe"
-  statut:string ="Offline"
-  age:number = Math.floor(Math.random()*100);
-  bio:string = "Je suis une bio"
-  reputation:string = ""
+  defaultImg: string = "https://picsum.photos/200/300"
+  img: string = "https://logowik.com/content/uploads/images/angular-a-letter-new-20243216.logowik.com.webp"
+  name: string = "John Doe"
+  statut: boolean = false
+  age: number = 25;
+  bio: string = "Je suis une bio"
+  reputation: string = ""
+  isRingardColor = "green";
+listFriend: Friend[] = [
+  {
+    img: "",
+    name: "",
+    statut: false,
+    age: Math.floor(Math.random() * 100),
+    bio: "",
+    reputation: "",
+  }
+];
+
+  radomizeAge(): number {
+    return this.age = Math.floor(Math.random() * 100);
+  }
+  regexIMG(img: string): boolean {
+    const regex = /^https?:\/\/[^\s]+\.(?:jpg|jpeg|png|gif)$/i;
+    return regex.test(img);
+  }
+  getIMG() {
+    return this.regexIMG(this.img) ? this.img : this.defaultImg;
+  }
+  changeStatut(): void {
+    this.statut = !this.statut;
+  }
+    addFriend(img:string, name:string, statut:boolean, age:number, bio:string, reputation:string ){
+      this.listFriend.push({img, name, statut, age, bio, reputation })
+      console.log(this.listFriend);
+  }
 }
+
+
+
+// const listFriend = [{
+//   friend: {
+//     img: "",
+//     name: "Emilie",
+//     statut: false,
+//     age: 29,
+//     bio: "Aime les pieds",
+//     reputation: "",
+//   }
+// },
+// {
+//   friend: {
+//     img: "",
+//     name: "Gomarz",
+//     statut: false,
+//     age: 27,
+//     bio: "Aime les chips aux pipi",
+//     reputation: "",
+//   }
+// },
+// {
+//   friend: {
+//     img: "",
+//     name: "Thomas",
+//     statut: true,
+//     age: 25,
+//     bio: "Se casse les os toutes les 2 sec",
+//     reputation: "",
+//   }
+// }]
